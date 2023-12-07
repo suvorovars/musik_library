a8d385354e473f54b0873a70186d85f393e079d8
suvorovars <suvorovars@gmail.com>
2023-12-07 17:17:35 +0500

    работа над фронтендом

--- Python-front-end/home.py
+++ Python-front-end/home.py
@@ -6,8 +6,11 @@ from time import gmtime, strftime
 
 from generate_img import generate_image
 
+__click_disk = None
+
 
 def create_home_page():
+    click_disk = None
     response = requests.get('http://localhost:8000/api/get/strings')
 
     for disk in response.json():
@@ -18,17 +21,23 @@ def create_home_page():
             df = df.sort_values(by='number')[['number', 'track_title', 'performer_name', 'genre_title', 'duration']]
         with col1:
             img = generate_image(f"""{disk['disk_title']}
-            {disk['year']}
-            """)
+                        {disk['year']}
+                        """)
             st.image(img)
             _, button_col = col1.columns(2)
             with button_col:
                 if disk['strings']:
                     st.text(f"listening time is {strftime('%M:%S', gmtime(df['duration'].sum()))}")
-                st.button(f'Edit {disk["disk_title"]}')
+                st.button(f'Edit {disk["disk_title"]}', on_click=click_button, args=(disk['disk_title'], ))
 
         with col2:
             if disk['strings']:
                 st.table(df)
             else:
                 st.text("Пока нет ни одной песни")
+
+
+def click_button(args):
+    global __click_disk
+    __click_disk = args
+    print(args)
--- Python-front-end/music library.py
+++ Python-front-end/music library.py
@@ -1,21 +1,25 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-from generate_img import generate_image
-from home import create_home_page
+from home import create_home_page, __click_disk
+from edit_disk import create_edit_disk_page
 
 with open('./style.css') as f:
     css = f.read()
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-page = 'home'
+__page = 'home'
 
 
 if __name__ == '__main__':
-    selected = option_menu(None, ["Home", "Create Disk", "Edit Disks", "Search", 'Settings'],
+    selected = option_menu('Music Library', ["Home", "Create Disk", "Edit Disks", "Search", 'Settings'],
                             icons=['house', 'plus-circle', "pencil-square", "search", 'gear'],
                             menu_icon="cast", default_index=0, orientation="horizontal")
+
+    if selected == 'Edit Disk':
+        create_edit_disk_page()
+        print(selected)
     if selected == 'Home':
         create_home_page()
-    elif selected == 'Create Disk':
+    if selected == 'Create Disk':
         pass
